11c0c97c5f30c102b4bc3babc63edd72f140ba46
ishtiaqueht <ishtiaqueht@gmail.com>
2025-07-28 20:25:23 +0600

    FeaturedGardeners done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner from '../Components/Banner';
+import FeaturedGardeners from '../Components/FeaturedGardeners';
 
 const Home = () => {
     return (
         <div>
             <section className='mb-24'>
                 <Banner></Banner>
+            </section>
+            <section className='mb-24'>
+                <FeaturedGardeners></FeaturedGardeners>
             </section>
         </div>
     );
